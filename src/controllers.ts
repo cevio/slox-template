728248ed2077b93044abbf7cbeb5cf71475a213f
沈赟杰 <shenyunjie@znrank.com>
2021-10-12 15:41:21 +0800

    update

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,8 +1,6 @@
 import { createServer } from 'slox'
 import IndexController from './pages/index';
-import LiveController from './pages/live';
 
 export function controllerRegister(defineController: ReturnType<typeof createServer>['defineController']) {
   defineController(IndexController);
-  defineController(LiveController);
 }
